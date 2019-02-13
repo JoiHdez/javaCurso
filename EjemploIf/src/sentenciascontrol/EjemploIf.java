24da000e82801fb3e4714b394a1a6aa962ec5ffa
JoiHdez <joehdezcon@gmail.com>
2019-02-12 20:23:28 -0600

    Edi3

---
+++ EjemploIf/src/sentenciascontrol/EjemploIf.java
@@ -0,0 +1,12 @@
+package sentenciascontrol;
+
+public class EjemploIf {
+
+    public static void main(String[] args) {
+        int x = 10;
+        
+        if(x < 20){
+            System.out.println("X menos que 20\n");
+        }
+    }
+}
---
+++ EjemplosIfElse/src/sentenciascontrol/EjemplosIfElse.java
@@ -0,0 +1,12 @@
+package sentenciascontrol;
+public class EjemplosIfElse {
+    public static void main(String[] args) {
+        int  x = 30;
+        
+        if( x < 20){
+            System.out.println("X menos que 20\n");
+        }else{
+            System.out.println("X mayor que 20\n");
+        }
+    }
+}
